3da0564c89f4a94860ba15ea5168dc8bf6ffe5d6
unknown <zecicmirko@gmail.com>
2019-11-17 21:52:50 +0100

    Dodati submit/reset dugmici i provera unosa forme. Sredjena skalabilnost strane za ponudu.

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,7 +1,57 @@
 function initMap() {
-    var location = { lat: 44.815844, lng: 20.437061 };
+    var location = { lat: 44.804360, lng: 20.474730 };
 
     var map = new google.maps.Map(document.getElementById("map"), { zoom: 14, center: location });
 
     var marker = new google.maps.Marker({ position: location, map: map });
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateInputBox(message) {
+    if (message === "") {
+        return false;
+    }
+
+    return true;
+}
+
+function validateForm() {
+
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let subject = document.querySelector("#subject").value;
+    let desc = document.querySelector("#desc").value;
+
+    if (!validateInputBox(name)) {
+        alert("Niste uneli ime!");
+
+        return false;
+    }
+
+
+    if (!validateEmail(email)) {
+        alert("Pogresan format e-maila!");
+
+        return false;
+    }
+
+    if (!validateInputBox(subject)) {
+        alert("Niste uneli naziv teme!");
+
+        return false;
+    }
+
+    if (!validateInputBox(desc)) {
+        alert("Niste uneli poruku!");
+
+        return false;
+    }
+
+    alert("Poruka uspesno poslata:\n Ime: " + name + "\nE-mail: " + email + "\nTema: " + subject + "\nPoruka: " + message);
+
+    return true;
 }
---
+++ javascript/ponuda.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    $('#slick1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        pauseOnDotsHover: false,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows: false,
+        infinite: true
+    });
+
+    $('#slick2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        pauseOnDotsHover: false,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows: false,
+        infinite: true
+    });
+
+    $('#slick3').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        pauseOnDotsHover: false,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows: false,
+        infinite: true
+    });
+
+});
+
+
+$('#box1').hover(function () {
+    $(this).siblings().addClass('boxFaded');
+    $('#slick1').slick('slickPlay');
+    document.getElementById('text1').innerHTML = "Ovde stoji text novi kad se predje misem preko prvog diva";
+}, function () {
+    $(this).siblings().removeClass('boxFaded');
+    $('#slick1').slick('slickPause');
+    document.getElementById('text1').innerHTML = "Ovde stoji text kad se pomeri mis sa diva (prvi)";
+});
+
+$('#box2').hover(function () {
+    $(this).siblings().addClass('boxFaded');
+    $('#slick2').slick('slickPlay');
+    document.getElementById('text2').innerHTML = "Ovde stoji text novi kad se predje misem preko drugog diva";
+}, function () {
+    $(this).siblings().removeClass('boxFaded');
+    $('#slick2').slick('slickPause');
+    document.getElementById('text2').innerHTML = "Ovde stoji text kad se pomeri mis sa diva (drugi)";
+});
+
+
+$('#box3').hover(function () {
+    $(this).siblings().addClass('boxFaded');
+    $('#slick3').slick('slickPlay');
+}, function () {
+    $(this).siblings().removeClass('boxFaded');
+    $('#slick3').slick('slickPause');
+});
